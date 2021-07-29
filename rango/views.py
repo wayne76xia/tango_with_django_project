347570825478bd4839c4b4b349b51d773a3c3349
Shiyue Xia <2586437x@student.gla.ac.uk>
2021-07-30 01:13:28 +0800

    Chapter4.1 Shiyue Xia 2586437x

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+
+#def index(request):
+#   return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+  return HttpResponse("I says here is the xixi laji. <a href='/rango/'>Index</a>")
